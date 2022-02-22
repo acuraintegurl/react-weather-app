8c382510d17b54dacd0a8e6ca20a5ac201ccf5d9
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-02-22 19:10:38 +1100

    updated link

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
     <div className="App">
       <div>Weather app</div>
       <footer>
-        This project was coded by Esther Nitafan and is
+        This project was coded by Esther Nitafan and is{" "}
         <a
-          href="https://github.com/acuraintegurl/weather-react"
+          href="https://github.com/acuraintegurl/react-weather-app"
           target="_blank"
         >
           open-sourced
