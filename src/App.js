44fa9d4dbd60280d8b9d0f2e13a53de7e61bd3ba
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-02-23 17:51:52 +1100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div>Weather app</div>
-      <footer>
-        This project was coded by Esther Nitafan and is{" "}
-        <a
-          href="https://github.com/acuraintegurl/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <button className="btn btn-primary">Hello</button>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://esther.design" target="_blank">
+            Esther Nitafan
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/acuraintegurl/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
