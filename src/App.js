d47691886a13d0191893d04a670dee3664e4d157
acuraintegurl <39477899+acuraintegurl@users.noreply.github.com>
2022-03-05 09:31:04 +1100

    Weather Forecast and Icons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
@@ -8,7 +9,14 @@ function App() {
         <Weather defaultCity="New York" />
         <footer>
           This project was coded by{" "}
-          <a href="https://esther.design">Esther Nitafan</a> and is{" "}
+          <a
+            href="https://esther.design"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Esther Nitafan
+          </a>{" "}
+          and is{" "}
           <a href="https://github.com/acuraintegurl/react-weather-app">
             open-sourced
           </a>
